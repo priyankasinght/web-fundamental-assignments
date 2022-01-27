35cd6caff7301685f64916a64ba7f70fc978b1e6
priyankasinght <35567382+priyankasinght@users.noreply.github.com>
2022-01-28 00:45:26 +0530

    Add files via upload

---
+++ 25 jan 2022/assignment_1.js
@@ -0,0 +1,15 @@
+/* Write a program to cheak wheter the number is greater than or equal to 10 or not.*/
+
+var a=15
+if(a>10)
+{
+console.log("Number is greater than 10")
+}
+else if (a==10)
+{
+console.log("Number is eqal to 10")
+}
+else
+{
+console.log("Number is less than 10")
+}
---
+++ 25 jan 2022/assignment_2.js
@@ -0,0 +1,19 @@
+/* Take a three number and write a program 
+to find maximum of those three number*/
+
+var a=25
+var b=10
+var c=35
+
+if(a>=b && a>=c)
+{
+console.log("Maximum number is ",a)
+}
+else if(b>=a && b>=c)
+{
+console.log("Maximum number is ",b)
+}
+else
+{
+console.log("Maximum number is ",c)
+}
